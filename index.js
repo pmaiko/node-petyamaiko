fd5cdf62555acb9aabd47423af99c746e549bd8c
petyamaiko <petyamaiko@gmail.com>
2023-02-12 10:36:33 +0200

    socket

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server, {
   cors: {
-    origin: "*:*",
+    origin: "*",
 
   }
   // extraHeaders: {
