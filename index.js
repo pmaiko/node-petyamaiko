8a5224a68128510332b3df2528adbbc0ac8f0650
petyamaiko <petyamaiko@gmail.com>
2023-02-12 10:33:33 +0200

    socket

--- index.js
+++ index.js
@@ -12,17 +12,11 @@ const cors = require('cors')
 app.use(cors({origin: '*'}))
 const server = http.createServer(app)
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
 const { Server } = require("socket.io")
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
+    origin: "*:*",
+
   }
   // extraHeaders: {
   //   'Access-Control-Allow-Origin': '*',
