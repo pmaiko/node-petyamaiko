02faef71b7372e0c47e607afd8807abd026e5bab
petyamaiko <petyamaiko@gmail.com>
2023-02-12 10:28:35 +0200

    socket

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-app.use(cors({origin: '*', exposedHeaders: {'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'}}))
+app.use(cors({origin: '*'}))
 const server = http.createServer(app)
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 const { Server } = require("socket.io")
 const io = new Server(server, {
   cors: {
-    origin: "https://front-petyamaiko.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"]
   }
   // extraHeaders: {
